f8936b3960be73e1fbb85c8178f8329c6579001a
Thomas Svartbäck <thomsva@gmail.com>
2019-02-19 09:36:14 +0200

    6.5 valmis

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -28,11 +28,12 @@ const reducer = (state = initialState, action) => {
       const id = action.data.id
       const anecdoteToChange = state.find(n => n.id === id)
       const changedAnecdote = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1 }
-      return state.map(anecdote =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      )
+      return state
+        .map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+        .sort((a1, a2) => (a2.votes - a1.votes))
     case 'NEW_ANECDOTE':
       state = state.concat(action.data)
+        .sort((a1, a2) => (a2.votes - a1.votes))
       return state
     default:
       return state
